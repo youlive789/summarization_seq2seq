82c24cae33e71e33db81eb71437f72bc5f4a1200
gwkim <youlive789@gmail.com>
2020-08-20 14:14:01 +0900

    feat: Testing seq2seq models by scratch

--- model.py
+++ model.py
@@ -1,5 +1,12 @@
+import numpy as np
 import tensorflow as tf
+from tensorflow.keras import Input
+from tensorflow.keras.layers import *
 
 class PilotModel(tf.keras.Model):
     def __init__(self):
         super(PilotModel, self).__init__(name="PilotModel")
+
+
+if __name__ == "__main__":
+    pass
